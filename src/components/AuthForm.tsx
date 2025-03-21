7b08a608e7a05321db0caaa930e1f9a886cf352a
Prashant29062003 <112088696+Prashant29062003@users.noreply.github.com>
2025-03-21 18:05:00 +0000

    initial-commit

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from 'react'
+
+const AuthForm = ({children}: {children: ReactNode}) => {
+  return (
+    <div className='auth-layout'>AuthForm</div>
+  )
+}
+
+export default AuthForm
